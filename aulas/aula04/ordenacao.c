905083a2390d8b4745e4a05b913dcd525be29e20
Guilherme <guicmamorim@gmail.com>
2026-04-10 11:45:04 +0000

    aula04

---
+++ aulas/aula04/ordenacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "ordenacao.h"
+
+void troca(int *a, int *b) {
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void bubble_sort(int v[], int n) {
+
+}
---
+++ aulas/aula04/ordenacao.h
@@ -0,0 +1,7 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+void troca(int &a, int &b);
+void bubbleSort(int arr[], int n);
+
+#endif
